bd5880c258352469921db0cf1fcd704dc522f37c
Monika09-byte <monika.matkar24@vit.edu>
2026-01-18 12:03:28 +0530

    Add weather condition + enter key search

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
-
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 const result = document.getElementById("result");
-const apiKey = "34c5e811c0cf1303f342ba6f0ac73a4c";
 
-async function getWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// 1) Convert city name -> latitude & longitude
+async function getCoordinates(city) {
+  const geoURL = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`;
 
-  const response = await fetch(url);
+  const response = await fetch(geoURL);
   const data = await response.json();
 
-  console.log(data);
+  // if city not found
+  if (!data.results || data.results.length === 0) {
+    result.innerHTML = `<p>City not found 😭</p>`;
+    return;
+  }
+
+  const place = data.results[0];
+  const latitude = place.latitude;
+  const longitude = place.longitude;
+
+  getWeather(latitude, longitude, place.name, place.country);
+}
+
+// 2) Use latitude & longitude -> fetch current weather
+async function getWeather(lat, lon, cityName, country) {
+  const weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+
+  const response = await fetch(weatherURL);
+  const data = await response.json();
+
+  const current = data.current_weather;
+  const dateObj = new Date(current.time);
+const niceTime = dateObj.toLocaleString();
+
+  const code = current.weathercode;
+
+let condition = "Unknown";
+
+if (code === 0) condition = "Clear Sky ☀️";
+else if (code === 1 || code === 2) condition = "Partly Cloudy 🌤️";
+else if (code === 3) condition = "Cloudy ☁️";
+else if (code >= 51 && code <= 67) condition = "Drizzle 🌦️";
+else if (code >= 71 && code <= 77) condition = "Snow ❄️";
+else if (code >= 80 && code <= 82) condition = "Rain 🌧️";
+else if (code >= 95) condition = "Thunderstorm ⛈️";
+
+
+  result.innerHTML = `
+    <h2>${cityName}, ${country}</h2>
+    <p><b>Temperature:</b> ${current.temperature}°C</p>
+    <p><b>Wind Speed:</b> ${current.windspeed} km/h</p>
+    <p><b>Time:</b> ${niceTime}</p>
+
+    <p><b>Condition:</b> ${condition}</p>
+
+  `;
 }
 
 searchBtn.addEventListener("click", () => {
   const city = cityInput.value.trim();
 
   if (city === "") {
-    result.innerHTML = "<p>Please enter a city name </p>";
+    result.innerHTML = "<p>Please enter a city name</p>";
     return;
   }
 
-  getWeather(city);
+  result.innerHTML = "<p>Fetching weather... 🌦️</p>";
+  getCoordinates(city);
 });
 
-
-
+cityInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    searchBtn.click();
+  }
+});
